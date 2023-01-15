7c8ddf6d9ba031c2edd38ed605cf6d54b584542d
ksenecki <k.senecki@gmail.com>
2023-01-15 10:52:42 +0100

    hooki

--- tests/sklep.spec.ts
+++ tests/sklep.spec.ts
@@ -1,15 +1,55 @@
 import { test, expect } from "@playwright/test";
 
-test("First test", async ({ page }) => {
-  await page.goto("/");
-  const siteTitle = await page.locator("a.site-title");
-  await expect(siteTitle).toContainText("Generic Shop");
-});
+test.describe("Testy e2e Sklepu", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+    const siteTitle = await page.locator("a.site-title");
+    await expect(siteTitle).toContainText("Generic Shop");
+  });
 
-test("Open Most Wanted category", async ({ page }) => {
-  await page.goto("/");
-  await page.click('a[title="Most Wanted"]');
-  await expect(page).toHaveURL("/product-category/most-wanted/");
+  test("Open Most Wanted category", async ({ page }) => {
+    await page.click('a[title="Most Wanted"]');
+    await expect(page).toHaveURL("/product-category/most-wanted/");
+  });
+
+  test("Empty login form", async ({ page }) => {
+    await page.click(".top-account");
+    await page.click('input[name="login"]');
+    const errorMessage = await page.locator(".woocommerce-error li");
+    await expect(errorMessage).toContainText("Error: Username is required.");
+  });
+
+  test("Login form", async ({ page }) => {
+    await page.click(".top-account");
+    await page.type("#username", "email@example.com", { delay: 100 });
+    await page.type("#password", "password", { delay: 100 });
+    await page.click('input[name="login"]');
+    const errorMessage = await page.locator(".woocommerce-error li");
+    await expect(errorMessage).toContainText(
+      "Error: A user could not be found with this email address."
+    );
+  });
+
+  test("Lost password invalid email @pass", async ({ page }) => {
+    await page.click(".top-account");
+    await page.click("text=Lost your password?");
+    const emailInput = await page.locator("#user_login");
+    const resetPasswordButton = await page.locator("[value='Reset password']");
+    await emailInput.fill("email@example.com");
+    await resetPasswordButton.click();
+    const errorMessage = await page.locator(".woocommerce-error li");
+    await expect(errorMessage).toContainText("Invalid username or email.");
+  });
+
+  test("Assercje", async ({ page }) => {
+    await expect(page).toHaveTitle("Generic Shop – Just another web shop");
+    await expect(page.locator(".site-description")).toBeVisible();
+    await expect(page.locator("h1")).not.toBeVisible();
+    await expect(page.locator(".site-description")).toHaveText(
+      "Just another web shop"
+    );
+    await expect(page.locator(".site-description")).toHaveCount(1);
+  });
 });
 
 test.skip("Selektory", async ({ page }) => {
@@ -30,52 +70,7 @@ test.skip("Selektory", async ({ page }) => {
   await page.click('//h3[@class="widget-title"]');
 });
 
-test.describe("Login form tests", () => {
-  test("Empty login form", async ({ page }) => {
-    await page.goto("/");
-    await page.click(".top-account");
-    await page.click('input[name="login"]');
-    const errorMessage = await page.locator(".woocommerce-error li");
-    await expect(errorMessage).toContainText("Error: Username is required.");
-  });
-
-  test("Login form", async ({ page }) => {
-    await page.goto("/");
-    await page.click(".top-account");
-    await page.type("#username", "email@example.com", { delay: 100 });
-    await page.type("#password", "password", { delay: 100 });
-    await page.click('input[name="login"]');
-    const errorMessage = await page.locator(".woocommerce-error li");
-    await expect(errorMessage).toContainText(
-      "Error: A user could not be found with this email address."
-    );
-  });
-});
-
-test("Lost password invalid email @pass", async ({ page }) => {
-  await page.goto("/");
-  await page.click(".top-account");
-  await page.click("text=Lost your password?");
-  const emailInput = await page.locator("#user_login");
-  const resetPasswordButton = await page.locator("[value='Reset password']");
-  await emailInput.fill("email@example.com");
-  await resetPasswordButton.click();
-  const errorMessage = await page.locator(".woocommerce-error li");
-  await expect(errorMessage).toContainText("Invalid username or email.");
-});
-
-test("Assercje", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle("Generic Shop – Just another web shop");
-  await expect(page.locator(".site-description")).toBeVisible();
-  await expect(page.locator("h1")).not.toBeVisible();
-  await expect(page.locator(".site-description")).toHaveText(
-    "Just another web shop"
-  );
-  await expect(page.locator(".site-description")).toHaveCount(1);
-});
-
-test("Adnotacje", async ({ page }) => {
+test.skip("Adnotacje", async ({ page }) => {
   /*
   .skip() pomija test
   .only() uruchamia tylko ten jeden test
@@ -84,5 +79,11 @@ test("Adnotacje", async ({ page }) => {
   .slow() wydłuża trzykrotnie domyślny timeout
   .describe() grupuje testy
   .parallel() wykonuje testy równolegle
+
+  Hooki:
+  .beforeEach() przed każdym testem
+  .beforeAll() przed wszystkimi testami
+  .afterEach() po każdym tescie
+  .afterAll() po wszystkich testach
   */
 });
