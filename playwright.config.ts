d38e5fc902788031871f0faf36a94458d2d6b246
ksenecki <k.senecki@gmail.com>
2023-03-12 10:27:24 +0100

    update reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   timeout: 30000,
   retries: 0,
-  reporter: process.env.CI ? "github" : "html",
+  reporter: process.env.CI ? "github" : "line",
   testDir: "tests/e2e",
   use: {
     headless: true,
